5944c24d7923cd1ad60569387cf89ad50cdb03a3
Max Rozen <rozenmd@gmail.com>
2017-12-09 11:01:08 +1100

    use font-awesome, style some stuff

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,16 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Link from "gatsby-link"
-import Helmet from "react-helmet"
-import ReactGA from "react-ga"
-ReactGA.initialize("UA-42211308-2")
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+import ReactGA from 'react-ga'
+ReactGA.initialize('UA-42211308-2')
 
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "./clean-blog.css"
-import "./custom.css"
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './clean-blog.css'
+import './custom.css'
+import fontawesome from '@fortawesome/fontawesome'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faGithub } from '@fortawesome/fontawesome-free-brands'
 
 const Header = () => (
   <div className="container">
@@ -15,16 +18,18 @@ const Header = () => (
     <div>
       <ul className="home-nav">
         <li key={0}>
-          <Link to="/">Home</Link>
+          <Link className="custom-link" to="/">
+            Home
+          </Link>
         </li>
         <li key={1}>
           <a target="_blank" href="https://www.linkedin.com/in/rozenmd">
-            LinkedIn
+            <FontAwesomeIcon icon={faLinkedinIn} />
           </a>
         </li>
         <li key={2}>
           <a target="_blank" href="https://github.com/rozenmd">
-            GitHub
+            <FontAwesomeIcon icon={faGithub} />
           </a>
         </li>
       </ul>
@@ -40,18 +45,18 @@ const TemplateWrapper = ({ children }) => (
       title="Max Rozen - Software Engineer - Portfolio"
       meta={[
         {
-          name: "description",
-          content: "Max Rozen - Software Engineer - Blog",
+          name: 'description',
+          content: 'Max Rozen - Software Engineer - Blog',
         },
-        { name: "keywords", content: "Max Rozen, Software Engineer, Blog" },
+        { name: 'keywords', content: 'Max Rozen, Software Engineer, Blog' },
       ]}
     />
     <Header />
     <div
       style={{
-        margin: "0 auto",
+        margin: '0 auto',
         maxWidth: 960,
-        padding: "0px 1.0875rem 1.45rem",
+        padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
       }}
     >
